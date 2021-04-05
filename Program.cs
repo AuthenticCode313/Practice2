1eb515e912e1d11e30f4c4db00b7690e8d25f245
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-04-05 08:14:54 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get length of rectangle
+            // get width of rectangle
+            // Calculate area and perimeter
+            // display those values
+            // make new github
+            // push to github
+
+            Console.WriteLine("What is the length of your rectangle?" );
+            string length = Console.ReadLine();
+            double length1 = double.Parse(length);
+            Console.WriteLine("What is the width of the rectangle");
+            string width = Console.ReadLine();
+            double width1 = double.Parse(width);
+
+            double area = length1 * width1;
+            double perimeter = (length1 * 2) + (width1 * 2);
+
+            Console.WriteLine("The area of the rectangle is " + area + " and the perimeter of the rectangle is " + perimeter);
+
+
+        }
+    }
+}
